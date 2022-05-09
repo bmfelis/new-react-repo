466ca3e677871f2375fce3d21d52790ba9ba154a
Wuttipat Jongrakthanakit <boom_49890@yahoo.com>
2022-05-09 15:04:20 +0700

    W2-02 Functional Components

--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -15,6 +15,7 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,
             </Card>
         );
     }
+    
     function renderDish(dish) {
         if (dish != null)
             return(
@@ -32,64 +33,29 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,
             );
     }
     
-class Menu extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedDish: null
-        }
-        
-    }
-    onDishSelect(dish){
-            this.setState({selectedDish: dish});
-        }
-    renderDish(dish){
-        if(dish != null)
-        return(
-            <Card key={dish.id}
-                        onClick={() => this.props.onClick(dish.id)}>
+    const Menu = (props) => {
 
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                      <CardTitle>{dish.name}</CardTitle>
-                      <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-        );
-        else
-        return(
-            <div></div>
-        );
-    }
-    render() {
-        const menu = this.props.dishes.map((dish) => {
+        const menu = props.dishes.map((dish) => {
             return (
-              <div className="col-12 col-md-5 m-1">
-                <Card key={dish.id}
-                  onClick={() => this.onDishSelect(dish)}>
-                  <CardImg width="100%" src={dish.image} alt={dish.name} />
-                  <CardImgOverlay>
-                      <CardTitle>{dish.name}</CardTitle>
-                  </CardImgOverlay>
-                </Card>
-              </div>
+                <div className="col-12 col-md-5 m-1"  key={dish.id}>
+                    <RenderMenuItem dish={dish} onClick={props.onClick} />
+                </div>
             );
         });
-
-
+    
         return (
-          <div className="container">
-            <div className="row">
-              {menu}
-            </div>
-            <div className="row">
-                <div  className="col-12 col-md-5 m-1">
-                    {this.renderDish(this.props.dishes[this.props.selectedDish])}
+            <div className="container">
+                <div className="row">
+                    {menu}
+                </div>
+           <div className="row">
+                  <div  className="col-12 col-md-5 m-1">
+                    {renderDish(props.dishes[props.selectedDish])}
+                  </div>
                 </div>
             </div>
-          </div>
         );
     }
-}
-
-export default Menu;
+    
+    export default Menu;
+    
